0ca069736817d2178f6c9a25f98cbfcf575032c7
ASAPMANU4 <manuel3000can@gmail.com>
2021-10-14 16:33:02 -0500

    Creacion del modelo
    se creo el modelo para la tabla de especies

---
+++ app/Especie.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Espécie extends Model
+{
+    protected $table='especies';
+    protected $primaryKey='id_especie';
+
+    //define si la llave primaria es o no un numero auto incrementable
+    public $incrementing=true;
+    //activar o desactrivar etiquetas de tiempo
+    public $timestamps=false;
+
+    public $fillable=[
+        'id_especie',
+        'especie',
+        
+    ];
+
+}
