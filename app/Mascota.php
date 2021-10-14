59b21af4889a1452932a040c600e94f898b50303
ASAPMANU4 <manuel3000can@gmail.com>
2021-10-14 17:05:17 -0500

    Creación de las relaciones
    Se creo la relacion varios a uno entre  la tabla Macotas a las tablas Especies y Razas, usando la relacion belongsTo.

--- app/Mascota.php
+++ app/Mascota.php
@@ -9,6 +9,12 @@ class Mascota extends Model
    protected $table='mascotas';
     protected $primaryKey='id_mascota';
 
+
+    //especificamos las relaciones
+    public $with=['especie'];
+
+    public $with=['raza'];
+
     //define si la llave primaria es o no un numero auto incrementable
     public $incrementing=false;
     //activar o desactrivar etiquetas de tiempo
@@ -25,4 +31,11 @@ class Mascota extends Model
         'id_raza',
         
     ];
+
+    public function especie(){
+      return $this->belongsTo(Especie::class, 'id_especie', 'id_especie');
+    }
+        public function raza(){
+      return $this->belongsTo(Raza::class, 'id_raza', 'id_raza');
+    }
 }
