f6610aa967d6e468fff412aeb790144a0089c580
ASAPMANU4 <manuel3000can@gmail.com>
2021-11-24 20:48:32 -0600

    vista Productos
    se creo un metodo que trae los productos de nuestra base datos, al igual que una vista para poder mostrar estos de mejor manera

---
+++ public/js/apis/apiVenta.js
@@ -0,0 +1,54 @@
+	function init() {
+var apiProducto='http://localhost/mascotas_cantamay/public/apiProducto';
+new Vue ({
+
+	//asignamos el TOKEN 
+	http: {
+            headers: {
+                'X-CSRF-TOKEN': document.querySelector('#token').getAttribute('value')
+            }
+        },
+
+	el:'#apiVenta',
+
+	data:{
+		mensaje:"Hola mundo",
+		sku:'',
+		ventas:[],
+
+	},
+	//se ejcuta automaticamente cuando se crea la pagina
+	created:function(){
+
+	},
+
+	//inicio de methods
+	methods:{
+		buscarProducto:function(){
+			if (this.sku)
+			{
+			var producto = {}
+			this.$http.get(apiProducto + '/' + this.sku).then(function(j){
+				producto = {
+					sku:j.data.sku,
+					nombre:j.data.nombre,
+					precio:j.data.precio,
+					cantidad:1,
+					total:j.data.precio
+				};
+
+				this.ventas.push(producto);
+				this.sku='';
+			});
+		}
+	}
+
+
+	},//fin de methdos
+	computed:{
+
+	},
+})
+
+} window.onload = init;
+
--- resources/views/ventas.blade.php
+++ resources/views/ventas.blade.php
@@ -8,14 +8,39 @@
 		<div class="col-md-4">
 
 			<div class="input-group mb-3">
-				<input type="text" placeholder="Escriba el sku del producto" class="form-control">
+				<input type="text" placeholder="Escriba el sku del producto" class="form-control" v-model="sku" v-on:keyup.enter="buscarProducto()">
 				<div class="input-group-append">
-				<button class="btn btn-primary">Buscar</button>		
+				<button class="btn btn-primary" @click="buscarProducto()">Buscar</button>		
 				</div>	
 			</div>
 
 		</div>
 	</div>
+
+<div class="row">
+	<div class="col-md-12">
+		<table class="table table-bordered">
+			<thead>
+				<th>SKU</th>
+				<th>producto</th>
+				<th>precio</th>
+				<th>cantidad</th>
+				<th>total</th>
+			</thead>
+			<tbody>
+				<tr v-for="venta in ventas">
+					<td>@{{venta.sku}}</td>
+					<td>@{{venta.nombre}}</td>
+					<td>@{{venta.precio}}</td>
+					<td>@{{venta.cantidad}}</td>
+					<td>@{{venta.total}}</td>
+				</tr>
+			</tbody>
+		</table>
+		
+	</div>
+</div>
+
 </div>
 <!-- fin de div principal -->
 
